2dba8be94ecf72dd7c8a910572a121532ef7814c
Sebastian Balan <sebastian.balan@nuance.com>
2020-03-30 13:04:22 +0300

    Added reset root and simple copy script

---
+++ simplecopy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#A simple copy script
+cp $1 $2
+#Let`s verify the copy worked
+echo Details for $2
+ls -lh $2
+
+#Line 3 - run the command cp with the first command line argument as the source and the second command line argument as the destination.
+#Line 5 - run the command echo to print a message.
+#Line 6 - After the copy has completed, run the command ls for the destination just to verify it worked. Included the options l (human readable)
